7686fd565b70fd45765fb4d5c3f7d5f207cb0cf1
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2020-03-17 11:57:27 +0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({
+        hi: 'there',
+        hello: 'too'
+    });
 });
 
 const PORT = process.env.PORT || 5000;
