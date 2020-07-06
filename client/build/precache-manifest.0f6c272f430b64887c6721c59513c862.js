3afb2d7c77662d84beaa8e6b8a44142543328cf3
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2020-07-06 21:10:50 +0700

    fixing version

---
+++ client/build/precache-manifest.0f6c272f430b64887c6721c59513c862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8341797b506d8308f7ba5d87745ef262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe13650abc1130eed634",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "fe13650abc1130eed634",
+    "url": "/static/js/2.0fca085c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0fca085c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c2ff9270661a1c7225",
+    "url": "/static/js/main.aef2e69e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
