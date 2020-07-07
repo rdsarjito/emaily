3ac7107c33fb15dc073a4958abcb5dc720b893ca
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2020-07-07 15:10:16 +0700

    make it better again again

--- routes/pricesRoutes.js
+++ routes/pricesRoutes.js
@@ -1,6 +1,6 @@
 const mid = require('../middleware');
 module.exports = (app) => {
-    app.get('/price', mid, (req, res) => {
-        res.send('all price');
-    })
+  app.get('/price', mid, (req, res) => {
+    res.send('all price');
+  })
 }
